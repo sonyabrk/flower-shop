dac0a3f8637c5c6ebe680317b85b52a9a2ea205f
sS <ss@MacBook-sS.local>
2026-09-22 17:19:34 +0300

    Добавлена сущность для заказа клиента

---
+++ src/main/java/flowershop/model/Customer.java
@@ -0,0 +1,82 @@
+package src.main.java.flowershop.model;
+
+import java.util.Objects;
+
+public class Customer {
+
+    private Long id;
+    private String fullName;
+    private String phone;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(String fullName, String phone, String email) {
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Customer(Long id, String fullName, String phone, String email) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
