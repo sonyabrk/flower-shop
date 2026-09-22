2c29d9013df9ccd2e3808886d3c3a0a48f06428b
sS <ss@MacBook-sS.local>
2026-09-22 17:17:16 +0300

    Добавлена сущность для букета

---
+++ src/main/java/flowershop/model/Bouquet.java
@@ -0,0 +1,82 @@
+package src.main.java.flowershop.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Bouquet {
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    public Bouquet() {
+    }
+
+    public Bouquet(String name, String description, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Bouquet(Long id, String name, String description, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Bouquet)) return false;
+        Bouquet bouquet = (Bouquet) o;
+        return Objects.equals(id, bouquet.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Bouquet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
